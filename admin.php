40a3e2b46bbd5422b420efdfd77f46e345f9f0f1
wanghanna <hannawang0306@livecom>
2019-10-04 21:32:36 -0700

    版面美化

--- admin.php
+++ admin.php
@@ -17,19 +17,27 @@ $body = (empty($_GET['page'])) ? 'main' : $_GET['page'];
 *{
   font-family: 'Noto Sans TC', sans-serif;
 }
+#title{
+  font-weight:bolder;
+  background:#655c57;
+  padding:10px;
+}
+#title a{
+  text-decoration:none;
+  color:#fef0bf;
+}
 </style>
 </head>
-<body class="row flex-column align-items-center">
-  <div class="my-1 bg-light row m-0 p-0 container justify-content-between">
-<a href="?page=main" class="col bg-light px-3 py-1 text-center text-decoration-none">回首頁</a>
-<a href="?page=light" class="col bg-light px-3 py-1 text-center text-decoration-none">光源管理</a>
-<a href="?page=component" class="col bg-light px-3 py-1 text-center text-decoration-none">零組件管理</a>
-<a href="?page=news" class="col bg-light px-3 py-1 text-center text-decoration-none">最新消息管理</a>
-<button class="col btn btn-outline-warning mr-3 text-center decoration" onclick="document.location.href=('api.php?do=logout')">登出</button>
+<body class="container">
+  <div id="title">
+<a href="?page=main">回首頁</a>
+<a href="?page=light">光源管理</a>
+<a href="?page=component">零組件管理</a>
+<a href="?page=news">最新消息管理</a>
+<button onclick="document.location.href=('api.php?do=logout')">登出</button>
 </div>
 <hr />
 <?php include_once("a".$body.".php") ?>
-
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
